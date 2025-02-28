fff2beacdc0231c5bb0f1239a6a1ff90bfab552f
AlexanderA02 <106035161+AlexanderA02@users.noreply.github.com>
2025-02-28 11:55:18 +0100

    VueRouter added, no routes yet.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import router from "../routes/routes.js";
 import "./style.css";
 import App from "./App.vue";
 
-createApp(App).use(createPinia()).mount("#app");
+createApp(App).use(router).use(createPinia()).mount("#app");
