8b7119c7b31691183ed4d8d4421328b220a28d34
AlexanderA02 <106035161+AlexanderA02@users.noreply.github.com>
2025-02-28 12:00:31 +0100

    VueRouter fixed

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-const routes = createRouter({
+const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [],
 });
+
+export default router;
