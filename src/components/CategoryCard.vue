5c91d4339d5853f6a3b68a404fd0ccdf9be837cd
AlexanderA02 <106035161+AlexanderA02@users.noreply.github.com>
2025-03-04 15:26:00 +0100

    CategoryCard component basics completed.

---
+++ src/components/CategoryCard.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  category: String,
+  clothingImg: Image,
+});
+</script>
+
+<template>
+  <section id="card">
+    <div>
+      <h3>{{ category }}</h3>
+    </div>
+    <img :src="clothingImg" :alt="category" />
+  </section>
+</template>
+
+<style scoped>
+#card {
+  width: 207px;
+  height: 257px;
+
+  color: white;
+  position: relative;
+}
+
+img {
+  width: 100%;
+  height: 100%;
+  display: block;
+}
+
+div {
+  position: absolute;
+
+  width: 100%;
+  height: 100%;
+  text-align: center;
+
+  top: 0;
+  left: 0;
+
+  z-index: 2;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+h3 {
+  background-color: var(--dark3);
+  padding: 0.3em;
+}
+</style>
--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -2,6 +2,10 @@
 import { useRoute } from "vue-router";
 import { useUserstore } from "../../stores/userStore";
 import { ref, onMounted } from "vue";
+import CategoryCard from "../components/CategoryCard.vue";
+import Navbar from "../components/Navbar.vue";
+import Footer from "../components/Footer.vue";
+import SearchbarComp from "../components/SearchbarComp.vue";
 
 const userStore = useUserstore();
 const route = useRoute();
@@ -22,15 +26,17 @@ onMounted(() => {
 });
 </script>
 <template>
-  <h1>Shop</h1>
-
+  <SearchbarComp></SearchbarComp>
   <p>{{ route.params.shoptype }}</p>
-  <ul v-if="store">
-    <li v-for="item in makeUniqueArr(store[route.params.shoptype])">
-      Item name: {{ item.category }}
-      <img :src="item.img" :alt="item.category" />
-    </li>
-  </ul>
+  <div v-if="store" class="grid grid-cols-2">
+    <CategoryCard
+      :category="item.category"
+      :clothingImg="item.img"
+      v-for="item in makeUniqueArr(store[route.params.shoptype])"
+    />
+  </div>
+  <Footer></Footer>
+  <Navbar></Navbar>
 </template>
 <style scoped>
 li {
