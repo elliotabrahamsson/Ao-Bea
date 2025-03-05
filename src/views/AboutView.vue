378d1f61fe6bce5c1cc75a56168ba155c141e11c
fernstream <oliver.fernstrom@iths.se>
2025-03-05 15:39:45 +0100

    Sparar ändringar innan npm fix

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,17 +1,21 @@
 <script setup>
-    import Footer from '../components/Footer.vue';
-    import Navbar from '../components/Navbar.vue';
-    import SearchbarComp from '../components/SearchbarComp.vue';
-    import About from '../components/About.vue'
+import Footer from "../components/Footer.vue";
+import Navbar from "../components/Navbar.vue";
+import SearchbarComp from "../components/SearchbarComp.vue";
+import About from "../components/About.vue";
+import DropdownProd from "../components/DropdownProd.vue";
+import DropdownCare from "../components/DropdownCare.vue";
+import CarouselSuggestion1 from "../components/CarouselSuggestion1.vue";
 </script>
 
 <template>
-    <SearchbarComp/>
-    <About/>
-    <Footer/>
-    <Navbar/>
+  <SearchbarComp />
+  <About />
+  <DropdownProd />
+  <DropdownCare />
+  <CarouselSuggestion1 />
+  <Footer />
+  <Navbar />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
