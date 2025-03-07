52eba83631c92bddec94d97742939ed1c160cd9d
AntoniaNoaksson <antonianoaksson@gmail.com>
2025-03-07 12:34:20 +0100

    Done with the SelectedCatergy

--- routes/routes.js
+++ routes/routes.js
@@ -1,36 +1,40 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../src/views/HomeView.vue";
-import Shop from "../src/views/Shop.vue";
-import AboutView from "../src/views/AboutView.vue";
-
-import ProductPageView from "../src/views/ProductPageView.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../src/views/HomeView.vue';
+import Shop from '../src/views/Shop.vue';
+import AboutView from '../src/views/AboutView.vue';
+import CategoryView from '../src/views/CategoryView.vue';
+import ProductPageView from '../src/views/ProductPageView.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "Home",
-      component: HomeView,
-    },
-    {
-      path: "/shop/:shoptype",
-      name: "Shop",
-      component: Shop,
-    },
-    {
-      path: "/about",
-      name: "About",
-      component: AboutView,
-    },
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'Home',
+            component: HomeView
+        },
+        {
+            path: '/shop/:shoptype',
+            name: 'Shop',
+            component: Shop
+        },
+        {
+            path: '/about',
+            name: 'About',
+            component: AboutView
+        },
 
-    {
-      path: "/product/:id",
-      name: "Product",
-      component: ProductPageView,
-    },
-
-  ],
+        {
+            path: '/product/:id',
+            name: 'Product',
+            component: ProductPageView
+        },
+        {
+            path: '/shop/:shoptype/:category',
+            name: 'Category',
+            component: CategoryView
+        }
+    ]
 });
 
 export default router;
