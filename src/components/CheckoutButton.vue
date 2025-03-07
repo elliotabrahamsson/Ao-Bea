4404a751ec524579d8fffc435512969cab2239ca
elliotabrahamsson <elliotsson@outlook.com>
2025-03-07 10:39:38 +0100

    Fix product name typo, update size format, and enhance ProductPageView layout with new components

--- src/components/CheckoutButton.vue
+++ src/components/CheckoutButton.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div
-    class="bg-[var(--dark3)] relative min-w-[190px] m-[1em] flex justify-center items-center"
+    class="bg-[var(--dark3)] p-4 relative min-w-[190px] m-[1em] h-[44px] flex justify-center items-center"
   >
     <h3 class="text-white whitespace-nowrap text-[10px]">
       Lägg till i varukorg
