cff0f72d92bb95ada46324e752674321b40d3742
AntoniaNoaksson <antonianoaksson@gmail.com>
2025-03-09 10:43:43 +0100

    Finished with breadcrombs component and orderconfirmation compont and view . And implemented breadcrumbs to checkout and confirmationview

--- src/components/BreadCromb.vue
+++ src/components/BreadCromb.vue
@@ -4,17 +4,26 @@
     const route = useRoute();
     const category = route.params.category;
     const shop = route.params.shoptype;
-    console.log(shop);
 </script>
 
 <template>
-    <div class="mt-5">
+    <div class="mt-5 ml-3">
         <nav class="flex" aria-label="Breadcrumb">
             <ol
                 class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse"
             >
-                <li class="inline-flex items-center">
+                <li
+                    v-if="
+                        route.path !== '/checkout' &&
+                        route.path !== '/orderconfirmation'
+                    "
+                    class="inline-flex items-center"
+                >
                     <router-link
+                        v-if="
+                            route.path !== '/checkout' &&
+                            route.path !== '/orderconfirmation'
+                        "
                         to="/"
                         class="inline-flex items-center text-sm font-medium text-black dark:text-gray-400 dark:hover:text-white ml-5"
                     >
@@ -33,61 +42,113 @@
                     </router-link>
                 </li>
 
-                <li>
+                <li
+                    v-if="
+                        route.path !== '/checkout' &&
+                        route.path !== '/orderconfirmation'
+                    "
+                >
                     <div class="flex items-center">
                         <svg
-                            class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"
+                            class="w-[26px] h-[26px] text-gray-800 dark:text-white"
                             aria-hidden="true"
                             xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
                             fill="none"
-                            viewBox="0 0 6 10"
+                            viewBox="0 0 24 24"
                         >
                             <path
                                 stroke="currentColor"
                                 stroke-linecap="round"
                                 stroke-linejoin="round"
                                 stroke-width="2"
-                                d="m1 9 4-4-4-4"
+                                d="M19 12H5m14 0-4 4m4-4-4-4"
                             />
                         </svg>
                     </div>
                 </li>
-                <router-link
-                    :to="{
-                        path: `/shop/${shop}`
-                    }"
-                    class="inline-flex items-center text-sm font-medium text-black dark:text-gray-400 dark:hover:text-white"
+                <li
+                    v-if="
+                        route.path !== '/checkout' &&
+                        route.path !== '/orderconfirmation'
+                    "
                 >
-                    <div>
-                        <p v-if="shop === 'womens_fashion'">dammode</p>
-                        <p v-else>herrmode</p>
-                    </div>
-                    {{ shoptype }}
-                </router-link>
+                    <router-link
+                        :to="`/shop/${shop}`"
+                        class="inline-flex items-center text-sm font-medium text-black dark:text-gray-400 dark:hover:text-white"
+                        ><div>
+                            <p v-if="shop === 'womens_fashion'">dammode</p>
+                            <p v-else>herrmode</p>
+                        </div>
+                    </router-link>
+                </li>
                 <!-- Kategori-länk -->
-                <li aria-current="page">
+                <li
+                    v-if="
+                        route.path !== '/checkout' &&
+                        route.path !== '/orderconfirmation'
+                    "
+                >
                     <div class="flex items-center">
                         <svg
-                            class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"
+                            class="w-[26px] h-[26px] text-gray-800 dark:text-white"
                             aria-hidden="true"
                             xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
                             fill="none"
-                            viewBox="0 0 6 10"
+                            viewBox="0 0 24 24"
                         >
                             <path
                                 stroke="currentColor"
                                 stroke-linecap="round"
                                 stroke-linejoin="round"
                                 stroke-width="2"
-                                d="m1 9 4-4-4-4"
+                                d="M19 12H5m14 0-4 4m4-4-4-4"
                             />
                         </svg>
+
                         <span
                             class="inline-flex items-center text-sm font-medium text-black dark:text-gray-400 dark:hover:text-white ml-2"
                         >
                             <p>{{ category }}</p></span
                         >
                     </div>
+                </li>
+                <li>
+                    <router-link
+                        v-if="
+                            route.path === '/checkout' ||
+                            route.path === '/orderconfirmation'
+                        "
+                        to="/checkout"
+                        class="inline-flex items-center text-sm font-medium text-black dark:text-gray-400 dark:hover:text-white"
+                    >
+                        <svg
+                            class="w-[26px] h-[26px] text-gray-800 dark:text-white"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                        >
+                            <path
+                                stroke="currentColor"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M5 12h14M5 12l4-4m-4 4 4 4"
+                            />
+                        </svg>
+
+                        <span
+                            class="inline-flex items-center text-sm font-medium text-black dark:text-gray-400 dark:hover:text-white ml-2"
+                        >
+                            <p>Tillbaka till varukorgen</p></span
+                        >
+                    </router-link>
                 </li>
             </ol>
         </nav>
