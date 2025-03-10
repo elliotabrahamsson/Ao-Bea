e9a7355cb267f9fc516800af751e6b0c66500c25
elliotabrahamsson <elliotsson@outlook.com>
2025-03-10 13:52:36 +0100

    Checked radio button

--- src/components/CheckoutForm.vue
+++ src/components/CheckoutForm.vue
@@ -154,6 +154,7 @@ const scrollDown = () => {
           <input
             id="list-radio-id"
             type="radio"
+            checked
             value=""
             name="list-radio"
             class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 :focus:ring-blue-600 :ring-offset-gray-700 :focus:ring-offset-gray-700 focus:ring-2 :bg-gray-600 :border-gray-500"
